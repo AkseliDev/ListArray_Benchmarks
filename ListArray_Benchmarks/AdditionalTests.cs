bfa451b4b61391a2fed438fc81ccb2ab06521e51
Akseli <cakla001@edut.xamk.fi>
2022-01-26 00:44:28 +0200

    Add project files.

---
+++ ListArray_Benchmarks/AdditionalTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ListArray_Benchmarks;
+
+internal static class AdditionalTests {
+
+
+    /// <summary>
+    /// Method to ensure that fixed works for lists (via <see cref="CollectionsMarshal.AsSpan"/>)
+    /// </summary>
+    public static void Ensure_FixedWorks_List() {
+
+        // create a list
+        var list = new List<int>();
+
+        // add some elements to it
+        for(int i = 0; i < 10; i++) {
+            list.Add(i);
+        }
+
+        // lets print the list to be sure what it looks like
+        Console.WriteLine( string.Join(",", list) );
+
+
+        // we get the span using CollectionsMarshal
+        Span<int> span = CollectionsMarshal.AsSpan(list);
+
+        unsafe {
+
+            // we use fixed statement on the span to get the pointer
+            fixed(int* pointer = span) {
+
+                // we initiate a garbage collection to try make the GC move the lists internal array
+                System.GC.Collect();
+
+                // lets log the stuff
+                for(int i = 0; i < span.Length; i++) {
+                    Console.Write($"{pointer[i]},");
+                }
+            }
+        }
+
+        // then we can observe the results in console
+
+        /**
+         * @compiled-result:
+         * 
+         * 0,1,2,3,4,5,6,7,8,9
+         * 0,1,2,3,4,5,6,7,8,9
+         * 
+         * Conclusion: Fixed works as intended on a list via CollectionsMarshal
+         */
+    }
+
+}
---
+++ ListArray_Benchmarks/Benchmarking.cs
@@ -0,0 +1,203 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using ListArray_Benchmarks;
+using System.Runtime.InteropServices;
+
+
+// AdditionalTests.Ensure_FixedWorks_List();
+
+BenchmarkRunner.Run<Benchmarking>();
+
+
+public class Benchmarking {
+
+    const int ArraySize = 1_000_000;
+
+
+    List<int> intList;
+    
+    int[] intArray;
+
+    public Benchmarking() {
+        intList = Enumerable.Range(0, ArraySize).ToList();
+        intArray = intList.ToArray();
+    }
+
+
+    #region List Methods
+
+    [Benchmark]
+    public int List_ForEach() {
+        int result = 0;
+        foreach (int i in intList) {
+            result += i;
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public int List_ForEach_Local() {
+        var localPin = intList;
+        int result = 0;
+        foreach (int i in localPin) {
+            result += i;
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public int List_For() {
+        int result = 0;
+        for(int i = 0; i < intList.Count; i++) {
+            result += intList[i];
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public int List_For_Local() {
+        var localPin = intList;
+        int result = 0;
+        for (int i = 0; i < localPin.Count; i++) {
+            result += localPin[i];
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public int List_For_Span() {
+        Span<int> span = CollectionsMarshal.AsSpan(intList);
+        int result = 0;
+        for (int i = 0; i < span.Length; i++) {
+            result += span[i];
+        }
+        return result;
+    }
+
+    #endregion
+
+
+    #region Array Methods
+
+    [Benchmark]
+    public int Array_ForEach() {
+        int result = 0;
+        foreach (int i in intArray) {
+            result += i;
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public int Array_ForEach_Local() {
+        var localPin = intArray;
+        int result = 0;
+        foreach (int i in localPin) {
+            result += i;
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public int Array_For() {
+        int result = 0;
+        for (int i = 0; i < intArray.Length; i++) {
+            result += intArray[i];
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public int Array_For_Local() {
+        var localPin = intArray;
+        int result = 0;
+        for (int i = 0; i < localPin.Length; i++) {
+            result += localPin[i];
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public int Array_For_Span() {
+        Span<int> span = intArray;
+        int result = 0;
+        for (int i = 0; i < span.Length; i++) {
+            result += span[i];
+        }
+        return result;
+    }
+
+    #endregion
+
+    #region Unsafe Methods
+    [Benchmark]
+    public int ArrayUnsafe_For_Fixed_Reverse() {
+        int result = 0;
+        unsafe {
+            fixed (int* arr = intArray) {
+                int* pointer = arr;
+                for (int i = intArray.Length; i > 0; i--) {
+                    result += *pointer;
+                    pointer++;
+                }
+            }
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public int ArrayUnsafe_For_Fixed_AddrLT() {
+        int result = 0;
+        unsafe {
+            fixed (int* arr = intArray) {
+                int* ptr = arr;
+                int* endPtr = arr + intArray.Length;
+                while (ptr < endPtr) {
+                    result += *ptr;
+                    ptr++;
+                }
+            }
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public int ListUnsafe_For_Fixed_Reverse() {
+
+        Span<int> span = CollectionsMarshal.AsSpan(intList);
+
+        int result = 0;
+
+        unsafe {
+            fixed (int* arr = span) {
+                int* pointer = arr;
+                for (int i = span.Length; i > 0; i--) {
+                    result += *pointer;
+                    pointer++;
+                }
+            }
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public int ListUnsafe_For_Fixed_AddrLT() {
+
+        Span<int> span = CollectionsMarshal.AsSpan(intList);
+
+        int result = 0;
+
+        unsafe {
+            fixed (int* arr = span) {
+                int* ptr = arr;
+                int* endPtr = arr + span.Length;
+                while (ptr < endPtr) {
+                    result += *ptr;
+                    ptr++;
+                }
+            }
+        }
+        return result;
+    }
+    #endregion
+}
